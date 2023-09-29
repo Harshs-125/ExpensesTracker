059d3bc9ef8202cfb6159d94513aaa3bca8c2c6e
harshsoni <harsh.soni@joshtsoftware.com>
2023-09-29 14:50:35 +0530

    username validation

--- expenseswebsite/authentication/urls.py
+++ expenseswebsite/authentication/urls.py
@@ -1,7 +1,9 @@
-from .views  import RegisterationView
+from .views  import RegisterationView,UsernameFieldValidation
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    path('register',RegisterationView.as_view(),name="register")
+    path('register',RegisterationView.as_view(),name="register"),
+    path('validate-username',csrf_exempt(UsernameFieldValidation.as_view()),name="validate username")
     
 ]
--- expenseswebsite/authentication/views.py
+++ expenseswebsite/authentication/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+import json
 # Create your views here.
 
 class RegisterationView(View):
     def get(self,request):
         return render(request,'authentication/register.html')
+
+class UsernameFieldValidation(View):
+    def post(self,request):
+        data=json.loads(request.body)
+        username=data['username']
+        if not str(username).isalnum():
+            return JsonResponse({"username_error":"username must be alpha numeric"},status=409)
+        if User.objects.filter(username=str(username)).exists():
+            return JsonResponse({"username_error":"username already exist , choose another one"},status=409)
+        return JsonResponse({"username_valid":True},status=200) 
